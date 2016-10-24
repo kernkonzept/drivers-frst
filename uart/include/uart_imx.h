29db52ab12a9bf97fafda336a22de23ced258670
Adam Lackorzynski <adam@l4re.org>
2016-10-24 00:00:00 +0000

    UART: Add support for i.MX7
    Change-Id: Iba21df942d6fca1608dd1a5231e28dfdba4add93

--- uart/include/uart_imx.h
+++ uart/include/uart_imx.h
@@ -16,7 +16,14 @@ namespace L4
   class Uart_imx : public Uart
   {
   public:
-    enum platform_type { Type_imx21, Type_imx35, Type_imx51, Type_imx6 };
+    enum platform_type
+    {
+      Type_imx21,
+      Type_imx35,
+      Type_imx51,
+      Type_imx6,
+      Type_imx7,
+    };
     explicit Uart_imx(enum platform_type type)
        : _type(type) {}
     bool startup(Io_register_block const *);
@@ -54,6 +61,12 @@ namespace L4
   {
   public:
     Uart_imx6() : Uart_imx(Type_imx6) {}
+  };
+
+  class Uart_imx7 : public Uart_imx
+  {
+  public:
+    Uart_imx7() : Uart_imx(Type_imx7) {}
   };
 };
 
