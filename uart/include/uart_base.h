e6aa794cb0c64341ee39e44298c62bebb8d15344
Frank Mehnert <frank.mehnert@kernkonzept.com>
2020-10-12 00:00:00 +0000

    Uart: document Uart base class methods
    Change-Id: Ia3b1039054e16fc44e604468ebc27f629c2b1bcd

--- uart/include/uart_base.h
+++ uart/include/uart_base.h
@@ -32,22 +32,99 @@ namespace L4
     : _mode(~0U), _rate(~0U)
     {}
 
+    /**
+     * Start the UART driver.
+     *
+     * \param regs      IO register block of the UART.
+     * \retval true     Startup succeeded.
+     * \retval false    Startup failed.
+     */
     virtual bool startup(Io_register_block const *regs) = 0;
 
     virtual ~Uart() {}
+
+    /**
+     * Terminate the UART driver. This includes disabling of interrupts.
+     */
     virtual void shutdown() = 0;
+
+    /**
+     * Set certain parameters of the UART.
+     *
+     * \param m         UART mode. Depends on the hardware.
+     * \param r         Baud rate.
+     * \retval true     Mode setting succeeded (or was not performed at all).
+     * \retval false    Mode setting failed for some reason.
+     *
+     * \note Some drivers don't perform any mode setting at all and just return
+     *       true.
+     */
     virtual bool change_mode(Transfer_mode m, Baud_rate r) = 0;
+
+    /**
+     * Read a character from the UART.
+     *
+     * \param blocking  If true, wait until a character is available for
+     *                  reading. Otherwise do not wait and just return -1 if
+     *                  no character is available.
+     * \return          The actual character read from the UART.
+     */
     virtual int get_char(bool blocking = true) const = 0;
+
+    /**
+     * Check if there is at least one character available for reading from the
+     * UART.
+     *
+     * \return 0 if there is no character available for reading, !=0 otherwise.
+     */
     virtual int char_avail() const = 0;
+
+    /**
+     * Transmit a number of characters.
+     *
+     * \param s         Buffer containing the characters.
+     * \param count     Number of characters to transmit.
+     * \return          The number of successfully written characters.
+     */
     virtual int write(char const *s, unsigned long count) const = 0;
 
+    /**
+     * Acknowledge a received interrupt.
+     */
     virtual void irq_ack() {}
 
+    /**
+     * Enable the receive IRQ.
+     *
+     * \retval true     The RX IRQ was successfully enabled / disabled.
+     * \retval false    The RX IRQ couldn't be enabled / disabled. The
+     *                  driver does not support this operation.
+     */
     virtual bool enable_rx_irq(bool = true) { return false; }
+
+    /**
+     * Return the transfer mode.
+     *
+     * \return The transfer mode.
+     */
     Transfer_mode mode() const { return _mode; }
+
+    /**
+     * Return the baud rate.
+     *
+     * \return The baud rate.
+     */
     Baud_rate rate() const { return _rate; }
 
   protected:
+    /**
+     * Internal function transmitting each character one-after-another and
+     * finally waiting that the transmission did actually finish.
+     *
+     * \param s          Buffer containing the characters.
+     * \param count      The number of characters to transmit.
+     * \return           The number of successful written characters.
+     */
     template <typename Uart_driver>
     int generic_write(char const *s, unsigned long count) const
     {
