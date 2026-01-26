582dbc8aa5fce0e3cd192e2ad8cc2396a027f2b9
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2026-01-26 00:00:00 +0000

    [API] Install arch headers in common include directory
    The headers of the applicable ARCH-* subdirectory are installed into
    the same directory as the generic ones for the L4Re toolchain. All
    other ARCH-* directories are ignored. This implies that we have to
    untangle the x86 headers.
    
    Change-Id: I24422b50524cfdfe092ecca4d2a3eefd87c82ec8
    Signed-off-by: Jan Klötzke <jan.kloetzke@kernkonzept.com>

--- include/ARCH-amd64/asm_access.h
+++ include/ARCH-amd64/asm_access.h
@@ -8,9 +8,41 @@
 #pragma once
 
 #include <l4/sys/l4int.h>
-#include <x86/l4/drivers/asm_access.h>
 
 namespace Asm_access {
+
+inline
+l4_uint8_t
+read(l4_uint8_t const *mem)
+{
+  l4_uint8_t val;
+
+  asm volatile ("movb %[mem], %[val]" : [val] "=q" (val) : [mem] "m" (*mem));
+
+  return val;
+}
+
+inline
+l4_uint16_t
+read(l4_uint16_t const *mem)
+{
+  l4_uint16_t val;
+
+  asm volatile ("movw %[mem], %[val]" : [val] "=r" (val) : [mem] "m" (*mem));
+
+  return val;
+}
+
+inline
+l4_uint32_t
+read(l4_uint32_t const *mem)
+{
+  l4_uint32_t val;
+
+  asm volatile ("movl %[mem], %[val]" : [val] "=r" (val) : [mem] "m" (*mem));
+
+  return val;
+}
 
 inline
 l4_uint64_t
@@ -21,6 +53,27 @@ read(l4_uint64_t const *mem)
   asm volatile ("movq %[mem], %[val]" : [val] "=r" (val) : [mem] "m" (*mem));
 
   return val;
+}
+
+inline
+void
+write(l4_uint8_t val, l4_uint8_t *mem)
+{
+  asm volatile ("movb %[val], %[mem]" :[mem]  "=m" (*mem) : [val] "qi" (val));
+}
+
+inline
+void
+write(l4_uint16_t val, l4_uint16_t *mem)
+{
+  asm volatile ("movw %[val], %[mem]" : [mem] "=m" (*mem) : [val] "ri" (val));
+}
+
+inline
+void
+write(l4_uint32_t val, l4_uint32_t *mem)
+{
+  asm volatile ("movl %[val], %[mem]" : [mem] "=m" (*mem) : [val] "ri" (val));
 }
 
 inline
