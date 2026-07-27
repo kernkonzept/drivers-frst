3d950ffdcda53a8e91a9d117f2ede7c2138c1db1
Adam Lackorzynski <adam@l4re.org>
2026-07-27 00:00:00 +0000

    uart/16550: Add BCM2835 and AST compatibles
    Change-Id: Ia7150ff8284142afb50201dc902c0fd2fc4147c2
    Signed-off-by: Adam Lackorzynski <adam@l4re.org>

--- uart/src/uart_16550.cc
+++ uart/src/uart_16550.cc
@@ -168,6 +168,10 @@ static l4re_device_spec_dt_ids dt_ids[] = {
   { .compatible = "ns16550" },
   { .compatible = "ns16550a" },
   { .compatible = "fsl,ns16550" },
+  { .compatible = "brcm,bcm2835-aux-uart" },
+  { .compatible = "aspeed,ast2500-uart" },
+  { .compatible = "aspeed,ast2600-uart" },
+  { .compatible = "aspeed,ast2700-uart" },
   {},
 };
 
